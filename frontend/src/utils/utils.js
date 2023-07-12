1adfff1dc1f805ff92a9860b41887d05b7e3fa86
Dasha Savostina <moskvichka123@yandex.ru>
2023-07-12 14:41:00 +0300

    поменяла адрес фронта

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,7 +1,7 @@
 const apiData = {
-  link: 'http://mesto.mestomesto.nomoredomains.work/'
+  link: 'http://mesto.mestomesto.nomoredomains.work'
 }
 
-const url = "http://mesto.mestomesto.nomoredomains.work/";
+const url = "http://mesto.mestomesto.nomoredomains.work";
 
 export {apiData, url}
