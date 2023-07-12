2faf59ec20732e0ca048d17bfacb1b34847aaef9
Dasha Savostina <moskvichka123@yandex.ru>
2023-07-12 19:00:25 +0300

    правка пути краш теста

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ mongoose.connect(DB_URL);
 
 app.use(requestLogger);
 
-app.get(auth, '/crash-test', () => {
+app.get('/crash-test', () => {
   setTimeout(() => {
     throw new Error('Сервер сейчас упадёт');
   }, 0);
