6601e0b86933ed2470a558e80d7a709b127267d4
Dasha Savostina <moskvichka123@yandex.ru>
2023-07-12 14:00:27 +0300

    обновила корс

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
-app.use(cors({ origin: 'http://localhost:3001' }));
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
